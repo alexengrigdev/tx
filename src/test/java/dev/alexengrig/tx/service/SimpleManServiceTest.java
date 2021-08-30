b1bad04b47810947665311a1621ddd1e408d58ce
Grig Alex <alexengrigdev@gmail.com>
2021-08-31 02:02:46 +0300

    Fix tests

--- src/test/java/dev/alexengrig/tx/service/SimpleManServiceTest.java
+++ src/test/java/dev/alexengrig/tx/service/SimpleManServiceTest.java
@@ -4,6 +4,8 @@ import dev.alexengrig.tx.domain.Man;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -17,17 +19,24 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @Testcontainers
 class SimpleManServiceTest {
     @Container
-    static PostgreSQLContainer<?> postgresqlContainer = new PostgreSQLContainer<>("postgres")
-            .withDatabaseName("foo")
-            .withUsername("foo")
-            .withPassword("secret");
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres")
+            .withDatabaseName("txdb-it")
+            .withUsername("txdb-it")
+            .withPassword("txdb-it");
 
     @Autowired
     ManService service;
 
+    @DynamicPropertySource
+    static void postgresDatasourceSetup(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
     @Test
     void should_setup() {
-        assertTrue(postgresqlContainer.isRunning(), "PostgreSQL isn't running");
+        assertTrue(postgres.isRunning(), "PostgreSQL isn't running");
         assertNotNull(service, "ManService");
     }
 
