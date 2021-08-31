db97d3407ef034348b27cc8a9ae06a1c3751507d
Grig Alex <alexengrigdev@gmail.com>
2021-09-01 00:11:53 +0300

    Update ManRepository

--- src/main/java/dev/alexengrig/tx/repository/ManRepository.java
+++ src/main/java/dev/alexengrig/tx/repository/ManRepository.java
@@ -4,6 +4,9 @@ import dev.alexengrig.tx.entity.ManEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ManRepository extends JpaRepository<ManEntity, Long> {
+    Optional<ManEntity> findForUpdateById(Long id);
 }
