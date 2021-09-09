96fd83621c75ff1ee70c4a0d2e81654a0ba70b58
Grig Alex <alexengrigdev@gmail.com>
2021-09-09 17:20:19 +0300

    Add pessimistic-write lock

--- src/main/java/dev/alexengrig/tx/repository/ManRepository.java
+++ src/main/java/dev/alexengrig/tx/repository/ManRepository.java
@@ -2,11 +2,14 @@ package dev.alexengrig.tx.repository;
 
 import dev.alexengrig.tx.entity.ManEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
 import java.util.Optional;
 
 @Repository
 public interface ManRepository extends JpaRepository<ManEntity, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<ManEntity> findForUpdateById(Long id);
 }
