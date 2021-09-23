cd08052f56626f9d13111c7ec8bb04b62b82a3a0
Grig Alex <alexengrigdev@gmail.com>
2021-09-23 18:25:55 +0300

    Create sub-project - isolation

---
+++ isolation/src/main/java/dev/alexengrig/tx/isolation/IsolationApplication.java
@@ -0,0 +1,13 @@
+package dev.alexengrig.tx.isolation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsolationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsolationApplication.class, args);
+    }
+
+}
---
+++ isolation/src/test/java/dev/alexengrig/tx/isolation/IsolationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexengrig.tx.isolation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsolationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
