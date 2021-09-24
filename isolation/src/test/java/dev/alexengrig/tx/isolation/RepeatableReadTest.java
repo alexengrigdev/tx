14c0fd955a047a4af95be6e5809e0a5edc35ce88
Grig Alex <alexengrigdev@gmail.com>
2021-09-25 01:47:14 +0300

    Create RepeatableReadTest

---
+++ isolation/src/test/java/dev/alexengrig/tx/isolation/RepeatableReadTest.java
@@ -0,0 +1,283 @@
+package dev.alexengrig.tx.isolation;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SpringBootTest
+@Testcontainers
+@SuppressWarnings({"SqlResolve", "SqlNoDataSourceInspection"})
+public class RepeatableReadTest {
+    @Container
+    static final MySQLContainer<?> MYSQL = new MySQLContainer<>("mysql");
+
+    @Autowired
+    PersonRepository personRepository;
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    @Autowired
+    TransactionTemplate txTemplate;
+
+    @DynamicPropertySource
+    static void setMySQLDataSource(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", MYSQL::getJdbcUrl);
+        registry.add("spring.datasource.username", MYSQL::getUsername);
+        registry.add("spring.datasource.password", MYSQL::getDatabaseName);
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        jdbcTemplate.execute("""
+                CREATE TABLE person (
+                    id INT PRIMARY KEY,
+                    name TEXT NOT NULL
+                )
+                """);
+        txTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_REPEATABLE_READ);
+    }
+
+    @AfterEach
+    void afterEach() {
+        jdbcTemplate.execute("DROP TABLE person");
+    }
+
+    @Test
+    void should_load() {
+        assertTrue(MYSQL.isRunning(), "MySQL must be running");
+        assertNotNull(personRepository, "PersonRepository");
+        assertNotNull(jdbcTemplate, "JdbcTemplate");
+        assertNotNull(txTemplate, "TransactionTemplate");
+    }
+
+    @Test
+    @SneakyThrows({InterruptedException.class, ExecutionException.class})
+    void should_doesNot_dirtyRead() {
+        int personId = 1;
+        String personName = "Bill";
+        Person person = personRepository.insert(personId, personName);
+
+        CountDownLatch onRollback = new CountDownLatch(1);
+        CountDownLatch onCommit = new CountDownLatch(1);
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        executorService.execute(() -> txTemplate.executeWithoutResult(status -> {
+            System.out.println("1: Start in " + Thread.currentThread().getName());
+            personRepository.updateNameById(personId, "Dirty ".concat(personName));
+            System.out.println("1: Updated");
+            onRollback.countDown();
+            System.out.println("1: Release 2");
+            try {
+                System.out.println("1: Wait 2");
+                onCommit.await();
+                System.out.println("1: Released");
+            } catch (InterruptedException e) {
+                fail(e);
+            }
+            status.setRollbackOnly();
+            System.out.println("1: Rollback");
+        }));
+        Future<String> personNameFuture = executorService.submit(() -> txTemplate.execute(status -> {
+            System.out.println("2: Start in " + Thread.currentThread().getName());
+            try {
+                System.out.println("2: Wait 1");
+                onRollback.await();
+                System.out.println("2: Released");
+            } catch (InterruptedException e) {
+                fail(e);
+            }
+            String name = personRepository.selectById(personId).getName();
+            System.out.println("2: Selected");
+            System.out.println("2: Release 1");
+            onCommit.countDown();
+            System.out.println("2: Commit");
+            return name;
+        }));
+
+        executorService.shutdown();
+        if (!executorService.awaitTermination(3, TimeUnit.SECONDS)) {
+            executorService.shutdownNow();
+            fail("Timeout expired");
+        }
+
+        String newPersonName = personNameFuture.get();
+        assertEquals(personName, newPersonName, "Person's name");
+
+        Person samePerson = personRepository.selectById(personId);
+        assertEquals(person.getName(), samePerson.getName(), "Person's name");
+    }
+
+    @RepeatedTest(10)
+    @SneakyThrows(InterruptedException.class)
+    void should_doesNot_unrepeatableRead() {
+        Person jack = personRepository.insert(1, "Jack");
+        Person jacob = personRepository.insert(2, "Jacob");
+        Person john = personRepository.insert(3, "John");
+        String namePrefix = "Ja";
+        assertTrue(jack.getName().startsWith(namePrefix), "Jack's name starts with 'Ja'");
+        assertTrue(jacob.getName().startsWith(namePrefix), "Jacob's name starts with 'Ja'");
+        assertFalse(john.getName().startsWith(namePrefix), "John's name doesn't start with 'Ja'");
+
+        CountDownLatch onFirstFetch = new CountDownLatch(1);
+        CountDownLatch onSecondFetch = new CountDownLatch(1);
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        executorService.execute(() -> txTemplate.executeWithoutResult(status -> {
+            System.out.println("1: Start in " + Thread.currentThread().getName());
+            Supplier<Set<String>> personNamesSupplier = () -> {
+                List<Person> persons = personRepository.selectAllByNameStartsWith(namePrefix);
+                assertEquals(2, persons.size(), "Number of persons");
+                return persons.stream()
+                        .map(Person::getName)
+                        .collect(Collectors.toSet());
+            };
+            Set<String> names = personNamesSupplier.get();
+            System.out.println("1: Selected");
+            assertTrue(names.contains("Jack"), "List of name doesn't contain 'Jack'");
+            assertTrue(names.contains("Jacob"), "List of name doesn't contain 'Jacob'");
+            System.out.println("1: Release 2");
+            onFirstFetch.countDown();
+            try {
+                System.out.println("1: Wait 2");
+                onSecondFetch.await();
+                System.out.println("1: Released");
+            } catch (InterruptedException e) {
+                fail(e);
+            }
+            names = personNamesSupplier.get();
+            System.out.println("1: Selected again");
+            assertTrue(names.contains("Jack"), "List of name doesn't contain 'Jack'");
+            assertTrue(names.contains("Jacob"), "List of name doesn't contain 'Jacob'");
+            assertFalse(names.contains("Jackson"), "List of name contains 'Jackson'");
+        }));
+        executorService.execute(() -> {
+            txTemplate.executeWithoutResult(status -> {
+                System.out.println("2: Start in " + Thread.currentThread().getName());
+                try {
+                    System.out.println("2: Wait 1");
+                    onFirstFetch.await();
+                    System.out.println("2: Released");
+                } catch (InterruptedException e) {
+                    fail(e);
+                }
+                assertTrue(personRepository.updateNameById(jack.getId(), "Jackson"),
+                        "Update Jack's name");
+                System.out.println("2: Updated");
+                System.out.println("2: Commit");
+            });
+            System.out.println("2: Committed");
+            System.out.println("2: Release 1");
+            onSecondFetch.countDown();
+        });
+
+        executorService.shutdown();
+        if (!executorService.awaitTermination(3, TimeUnit.SECONDS)) {
+            executorService.shutdownNow();
+            fail("Timeout expired");
+        }
+
+        Person updatedJack = personRepository.selectById(jack.getId());
+        assertEquals("Jackson", updatedJack.getName(), "Updated Jack's name");
+    }
+
+    @Test
+    @SneakyThrows({InterruptedException.class, ExecutionException.class})
+    void should_do_phantomRead() {
+//TODO: Find DBMS with phantom read on repeatable isolation
+        Person tom = personRepository.insert(1, "Tom");
+        Person jerry = personRepository.insert(2, "Jerry");
+
+        List<Person> allPersons = personRepository.selectAll();
+        assertEquals(2, allPersons.size(), "Number of persons");
+
+        CountDownLatch onFirstFetch = new CountDownLatch(1);
+        CountDownLatch onSecondFetch = new CountDownLatch(1);
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        Future<List<Person>> personsFuture = executorService.submit(() -> txTemplate.execute(status -> {
+            System.out.println("1: Start in " + Thread.currentThread().getName());
+            List<Person> persons = personRepository.selectAll();
+            System.out.println("1: Selected");
+            assertEquals(2, persons.size(), "Number of persons");
+            assertTrue(persons.contains(tom), "List of persons contains 'Tom'");
+            assertTrue(persons.contains(jerry), "List of persons contains 'Jerry'");
+            System.out.println("1: Release 2");
+            onFirstFetch.countDown();
+            try {
+                System.out.println("1: Wait 2");
+                onSecondFetch.await();
+                System.out.println("1: Released");
+            } catch (InterruptedException e) {
+                fail(e);
+            }
+            persons = personRepository.selectAll();
+            System.out.println("1: Selected again");
+            //FIXME: Must be phantom read
+            //       assertEquals(3, persons.size(), "Number of persons");
+            assertEquals(2, persons.size(), "Number of persons");
+            System.out.println("1: Commit");
+            return persons;
+        }));
+        executorService.execute(() -> {
+            txTemplate.executeWithoutResult(status -> {
+                System.out.println("2: Start in " + Thread.currentThread().getName());
+                try {
+                    System.out.println("2: Wait 1");
+                    onFirstFetch.await();
+                    System.out.println("2: Released");
+                } catch (InterruptedException e) {
+                    fail(e);
+                }
+                personRepository.insert(3, "Spike");
+                System.out.println("2: Inserted");
+                System.out.println("2: Commit");
+            });
+            System.out.println("2: Committed");
+            System.out.println("2: Release 1");
+            onSecondFetch.countDown();
+        });
+
+        executorService.shutdown();
+        if (!executorService.awaitTermination(3, TimeUnit.SECONDS)) {
+            executorService.shutdownNow();
+            fail("Timeout expired");
+        }
+
+        List<Person> persons = personsFuture.get();
+        Set<String> names = persons.stream().map(Person::getName).collect(Collectors.toSet());
+        assertTrue(names.contains("Tom"), "List of names doesn't contain 'Tom'");
+        assertTrue(names.contains("Jerry"), "List of names doesn't contain 'Jerry'");
+//FIXME: Must be phantom read
+//       assertTrue(names.contains("Spike"), "List of names doesn't contain 'Spike'");
+        Person spike = personRepository.selectById(3);
+        assertNotNull(spike, "Spike");
+    }
+}
